0d18c444d93cfec3e169c116787585c3f677502f
vladismailov <izmaylov.vlad2001@gmail.com>
2024-05-14 16:18:24 +0300

    refactor(api): add api path
    Add createAlbum path into UrlPath class

--- lib/resources/url_path.dart
+++ lib/resources/url_path.dart
@@ -4,6 +4,7 @@ const String baseUrl = 'https://api.spotify.com/v1/';
 class UrlPath {
   static const String newReleases = 'browse/new-releases';
   static const String getAlbum = 'albums/{id}';
+  static const String createAlbum = 'albums/{id}';
 
   static const String severalTracks = '/tracks';
 }
